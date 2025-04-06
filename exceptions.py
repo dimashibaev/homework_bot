33fd831ddcebb102205bdf08cd86533a0e2f633c
Дмитрий Шибаев <dima.shibaev2004@gmail.com>
2025-04-07 00:21:45 +0300

    Доделал

--- exceptions.py
+++ exceptions.py
@@ -1,18 +1,14 @@
 class TokenMissingError(Exception):
     """Исключение, вызываемое при отсутствии переменных окружения."""
-    pass
 
 
 class APIRequestError(Exception):
     """Ошибка при запросе к API Практикума."""
-    pass
 
 
 class APIResponseFormatError(Exception):
     """Ответ от API не соответствует ожидаемому формату."""
-    pass
 
 
 class StatusUnknownError(Exception):
     """Неизвестный статус домашней работы."""
-    pass
